72ca891467aa37c0158408070becd0b1905982b3
Vasil Hadzhiev <vphadzhiev@gmail.com>
2021-10-28 16:04:28 +0300

    Add files via upload

---
+++ C01-Python-Basics/c01p02_sum_of_digits.py
@@ -0,0 +1,14 @@
+def sum_of_digits(n):
+    number = abs(n)
+    result = 0
+    while number:
+        result += abs(number % 10)
+        number = number // 10
+
+    return result
+
+
+print(sum_of_digits(1325132435356))
+print(sum_of_digits(123))
+print(sum_of_digits(6))
+print(sum_of_digits(-10))
