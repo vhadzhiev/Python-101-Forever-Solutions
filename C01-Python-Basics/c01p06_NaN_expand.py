d2ca30142635331e527f6452104d6717f4ca07e4
Vasil Hadzhiev <vphadzhiev@gmail.com>
2021-11-02 15:56:00 +0200

    Add files via upload

--- C01-Python-Basics/c01p06_NaN_expand.py
+++ C01-Python-Basics/c01p06_NaN_expand.py
@@ -1,7 +1,8 @@
 def nan_expand(times):
-    if times > 0:
-        return f"{times * 'Not a '}NaN"
-    return ""
+    if times == 0:
+        return ""
+
+    return f"{times * 'Not a '}NaN"
 
 
 print(nan_expand(0) == "")
